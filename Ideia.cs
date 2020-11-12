86ba183f93442d4f42726c88b1cc3f2892665620
Daniel de jesus moreira <danieljesuscmoreira@hotmail.com>
2020-11-12 01:05:12 +0000

    Main

--- Ideia.cs
+++ Ideia.cs
@@ -4,9 +4,32 @@ class ideia{
   public string area;
   private int ranking;
 
-  public string Getdescricao(){
+  public string Getdescricao() {
     return descricao;
   }
+  public int Getvotostotais() {
+    return votostotais;
+  }
+  public string Getarea() {
+    return area;
+  }
+  public int Getranking() {
+    return ranking;
+  }
+
+  public void Setdescricao(string dc){
+    descricao = dc;
+  }
+  public void Setvotostotais(int vt){
+    votostotais = vt;
+  }
+  public void Setarea(string a){
+    area = a;
+  }
+  public void Setranking(int r){
+    ranking = r;
+  }
+
   public ideia(string d,int vt, string a, int rnk){
    descricao = d;
    votostotais = vt;
--- main.cs
+++ main.cs
@@ -3,39 +3,56 @@ using System.Collections.Generic;
 
 class MainClass {
   public static void Main () { 
-    usuario anonimo = new usuario("anonimo",0,"Confirma e-mail",0);
-    ideia id = new ideia("ideia",0,"não especificada",1);   
+    List <ideia>   Lista_ideia    = new List<ideia>();
+
+       
     votacao vt = new votacao (0, "nenhum");
     string cadastrar = "S";
     while (cadastrar == "S"){
+      
+      usuario anonimo = new usuario("anonimo", 0,"Confirma e-mail", 0);
+      ideia   id      = new ideia  ("ideia", 0,"não especificada",  0);
+      // // // descricao, votostotais, area, ranking
+
       Console.WriteLine("Deseja cadastrar um novo usuario ? S/N");
       cadastrar = Console.ReadLine();
+      
+      
       if (cadastrar == "S"){
+
         Console.WriteLine("diga o nome>>");
         string nome = Console.ReadLine();
+        
         anonimo.Setnome(nome);
         Console.WriteLine("diga o email>>");
+
         string email = Console.ReadLine();
         anonimo.Setemail(email);
         Console.WriteLine ("diga o telefone>>");
+
         int telefone = int.Parse(Console.ReadLine());
         anonimo.Settelefone(telefone);
         anonimo.Setfundos(0);
+
         Console.WriteLine ("diga sua ideia");
-        id.descricao = Console.ReadLine();
+         string descricao= Console.ReadLine();
+         id.Setdescricao(descricao);
+
         Console.WriteLine("qual a aréa da sua ideia?");
-        id.area = Console.ReadLine();
+        string area = Console.ReadLine();
+        id.Setarea(area);
+
+        Lista_ideia.Add(id);
+        
         
       }
 
-    }
+    } 
 
-
-    List <ideia> Lista_ideia  = new List<ideia>();
     List <votacao> Lista_votacaos = new List<votacao>();
-    
+    for(int i = 0; i<Lista_ideia.Count; i++){
+      Console.WriteLine( Lista_ideia[i].Getdescricao() );
 
-    for(int i = 0; i<1; i++){
 
     }
   }
